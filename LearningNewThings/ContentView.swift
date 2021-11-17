171a940399e7d8f451f247c8ee6e40697bf60f35
Markus <markus@adop.cc>
2021-11-17 18:37:21 +0900

    Initial Commit

---
+++ LearningNewThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningNewThings
+//
+//  Created by ADOP_Mac on 2021/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningNewThings/LearningNewThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningNewThingsApp.swift
+//  LearningNewThings
+//
+//  Created by ADOP_Mac on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct LearningNewThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
